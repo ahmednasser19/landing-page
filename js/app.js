12fcb06263b7e0ca61b7488ef5a40dca90e2d77b
ahmed nasser <ahmednas197@gmail.com>
2021-04-21 00:55:39 +0200

    add navbar to the app

--- js/app.js
+++ js/app.js
@@ -1,57 +1,46 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-
-/**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
-
+ *
+ */
+const createFragment = document.createDocumentFragment();
+const scetions = document.querySelectorAll("section");
+function navBarItem(id, name) {
+  const navItem = `<a class ="menu__link" href='#${id}'>${name}</a>`;
+  return navItem;
+}
+function navBarCreation() {
+  for (let i = 0; i < scetions.length; i++) {
+    const newItem = document.createElement("li");
+    const itemName = scetions[i].getAttribute("data-nav");
+    const itemId = scetions[i].getAttribute("id");
+    newItem.innerHTML = navBarItem(itemId, itemName);
+    createFragment.appendChild(newItem);
+  }
+  const navBar_List = document.getElementById("navbar__list");
+  navBar_List.appendChild(createFragment);
+}
+navBarCreation();
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
-// Set sections as active
-
-
+// Set sections as activ
