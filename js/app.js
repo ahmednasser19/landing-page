6498e3c6e3145639a168f6abfdd10a5db6c399b1
ahmed nasser <ahmednas197@gmail.com>
2021-04-21 17:31:48 +0200

    fix issues

--- js/app.js
+++ js/app.js
@@ -18,12 +18,12 @@
  *
  */
 
-//create a document fregment for saving the new items of
-const createFragment = document.createDocumentFragment();
 //get all the sections i HTML page and save it in sections
 
-const scetionsList = document.querySelectorAll("section");
+const AllscetionsListItems = document.querySelectorAll("section");
 
+//create a document fregment for saving the new items of
+const createFragmentDoc = document.createDocumentFragment();
 /**
  * End Global Variables
  * Start Helper Functions
@@ -32,21 +32,11 @@ const scetionsList = document.querySelectorAll("section");
 
 // create an item in the nav bar and link it to its section in the page
 function navBarItem(id, name) {
-  const navItem = `<a class ="menu__link" href='#${id}'>${name}</a>`;
-  return navItem;
+  const navNewItem = `<a class ="menu__link" href='#${id}'>${name}</a>`;
+  return navNewItem;
 }
 //declearation if viewPort  function
-function viewPortSection(elem) {
-  let sectionBounding = elem.getBoundingClientRect();
-  return (
-    sectionBounding.top >= 0 &&
-    sectionBounding.left >= 0 &&
-    sectionBounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    sectionBounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -59,39 +49,44 @@ function viewPortSection(elem) {
 function navBarCreation() {
   //loop through the sections of our files
 
-  for (let i = 0; i < scetionsList.length; i++) {
+  for (let i = 0; i < AllscetionsListItems.length; i++) {
     // get the id and save it to the item id
-    const itemId = scetionsList[i].getAttribute("id");
+    const itemId = AllscetionsListItems[i].getAttribute("id");
     //create item for each one
     const newItem = document.createElement("li");
 
     //get item name from data-nav attribute
-    const itemName = scetionsList[i].getAttribute("data-nav");
+    const itemName = AllscetionsListItems[i].getAttribute("data-nav");
 
     newItem.innerHTML = navBarItem(itemId, itemName);
 
     //ad every new item in the navbar through the loop in createfragment
-    createFragment.appendChild(newItem);
+    createFragmentDoc.appendChild(newItem);
   }
   const navBar_List = document.getElementById("navbar__list");
 
   //add the the ites for the loop to the html navbar
-  navBar_List.appendChild(createFragment);
+  navBar_List.appendChild(createFragmentDoc);
 }
 
 navBarCreation();
 
 // Add class 'active' to section when near top of viewport
+
+function viewPortSection(elem) {
+  let sectionBoundingElement = elem.getBoundingClientRect();
+  return sectionBoundingElement.top >= 0 && sectionBoundingElement.left >= 0;
+}
 function setActiveClass() {
-  for (let i = 0; i < scetionsList.length; i++) {
+  for (let i = 0; i < AllscetionsListItems.length; i++) {
     //if the section is in veiw port
-    if (viewPortSection(scetionsList[i])) {
-      if (scetionsList[i].classList.contains("your-active-class")) {
+    if (viewPortSection(AllscetionsListItems[i])) {
+      if (AllscetionsListItems[i].classList.contains("your-active-class")) {
         //add your active class to the given view port
-        scetionsList[i].className = "";
+        AllscetionsListItems[i].className = "";
       } else {
         // remove that given class
-        scetionsList[i].className = "your-active-class";
+        AllscetionsListItems[i].className = "your-active-class";
       }
     }
   }
